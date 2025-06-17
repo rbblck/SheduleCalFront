99ada9ac76cdd3a8c98ea3db22b5243447dc1da1
Robert Black <rbblck@aol.com>
2025-06-17 07:56:13 +0300

    Added android app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.atlantatools.angular',
+  appName: 'atlantatools',
+  webDir: 'dist/shedule-cal-front'
+};
+
+export default config;
